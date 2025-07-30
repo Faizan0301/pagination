04cef9fac36b2a7a1730c017b7894daa6aa5c49e
Faizan0301 <152978292+Faizan0301@users.noreply.github.com>
2025-07-30 22:01:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import DataTable from './components/DataTable'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
-import './app.css'
+import './App.css'
 
 function App() {
 
